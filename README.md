fb0a05b57059df4a3b7a3617823d60a38b8bec0b
Rob Mercer <robmercer@Robs-MacBook-Pro.local>
2020-12-25 23:27:14 -0500

    1 - getting going

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # photoshare
+
+# Stupid simple photo sharing application. Share photos 
+# backed by s3 bucket with friends and family
+
+# Use case 1: I want to share photos in a large collection (album) with family members
+
+
+# photos - have an owner
+# albums - have an owner, multiple photos
+# users
+# groups - have permission to access albums
+
+
+running postgres:
+
+docker run --rm --name postgres-db -p 5432:5432 -e POSTGRES_PASSWORD=n0password1 -d postgres
+$> docker run -d -p 5432:5432 --name postures_db -e POSTGRES_PASSWORD=n0password1 postgres 
+$> docker ps # to get the ID used below:
+$> docker exec -it <ID> psql -U postgres
+
+psql -h localhost -p 5432 -U postgres -W
--- flask_app.py
+++ flask_app.py
@@ -9,11 +9,10 @@ app.secret_key = '$:c{%;cW=927DHTN'
 auth = flask_login.LoginManager()
 auth.init_app(app)
 
-users = {'foo@bar.tld': {'password': 'secret'}}
+users = {'bob': {'password': 'secret'}}
 
 class User(flask_login.UserMixin):
     pass
-
 
 @auth.user_loader
 def user_loader(email):
@@ -34,8 +33,6 @@ def request_loader(request):
     user = User()
     user.id = email
 
-        
-
     # DO NOT ever store passwords in plaintext and always compare password
     # hashes using constant-time comparison!
     user.is_authenticated = request.form['password'] == users[email]['password']
@@ -43,10 +40,11 @@ def request_loader(request):
     return user
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return '''
-               <form action='login' method='POST'>
+               <form action='/login' method='POST'>
                 <input type='text' name='email' id='email' placeholder='email'/>
                 <input type='password' name='password' id='password' placeholder='password'/>
                 <input type='submit' name='submit'/>
@@ -79,3 +77,23 @@ def unauthorized_handler():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # import sqlite3
+    #
+    # conn = sqlite3.connect('photoshare.db')
+    #
+    # c = conn.cursor()
+    #
+    # # Create table
+    # c.execute('''CREATE TABLE users
+    #              (id int, trans text, symbol text, qty real, price real)''')
+    #
+    # # Insert a row of data
+    # c.execute("INSERT INTO users VALUES ('','BUY','RHAT',100,35.14)")
+    #
+    # # Save (commit) the changes
+    # conn.commit()
+    #
+    # # We can also close the connection if we are done with it.
+    # # Just be sure any changes have been committed or they will be lost.
+    # conn.close()
